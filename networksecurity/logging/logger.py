3f22b491c324bce417736d5c4db5797a388426fd
SambhavSurthi <sambhavsurthi14@gmail.com>
2026-02-15 14:41:43 +0530

    Added logging and exception

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,20 @@
+import os
+import logging
+from datetime import datetime
+
+# Create logs directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Create log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
+logger = logging.getLogger("networksecurity")
